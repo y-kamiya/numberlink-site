9b5c0f0a5bad7914db8c681f1d958bcf3080e449
Yuji Kamiya <y.kamiya0@gmail.com>
2016-05-09 21:32:21 +0900

    return designated file

--- numberlink-site/src/Main.hs
+++ numberlink-site/src/Main.hs
@@ -6,6 +6,8 @@ import Network.Wai
 import Network.Wai.Handler.Warp (run)
 import Network.HTTP.Types (status200)
 import Network.HTTP.Types.Header (hContentType)
+import System.FilePath.Posix (takeFileName)
+import Data.ByteString.Char8 (unpack)
 
 main :: IO ()
 main = do
@@ -17,5 +19,8 @@ app :: Application
 app req f = do
     print $ requestMethod req
     print $ rawPathInfo req
-    f $ responseFile status200 [(hContentType, "text/html")] "a.html" Nothing
+    let filename = takeFileName $ unpack $ rawPathInfo req
+    f $ responseFile status200 [(hContentType, "text/html")] (publicDir ++ "/" ++ filename) Nothing
     -- f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
+
+publicDir = "resources"
