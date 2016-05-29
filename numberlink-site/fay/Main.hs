2b77affd4d2f4548599e73cf2b1e4744fc5790e4
Yuji Kamiya <y.kamiya0@gmail.com>
2016-05-29 14:14:43 +0900

    highlight current cell

--- numberlink-site/fay/Main.hs
+++ numberlink-site/fay/Main.hs
@@ -9,12 +9,16 @@ import           FFI
 import           Fay.Text (fromString)
 import qualified Fay.Text as T
 import Data.Char (ord)
+import Data.Function
 
 alert :: String -> Fay ()
 alert = ffi "alert(%1)"
 
 console :: T.Text -> Fay ()
 console = ffi "console.log(%1)"
+
+this :: Fay JQuery
+this = ffi "this"
 
 charCodeOffset :: Int
 charCodeOffset = 96
@@ -27,11 +31,18 @@ chunksOf n _ | n < 1 = []
 chunksOf n [] = []
 chunksOf n ns = take n ns : chunksOf n (drop n ns)
 
+buildTable :: (Int -> T.Text) -> Int -> Int -> T.Text
+buildTable f row col = let contents = T.concat $ map (buildRow f) $ chunksOf col [0..(row * col - 1)]
+                       in  T.concat ["<table>", contents, "</table>"]
+
+buildRow :: (Int -> T.Text) -> [Int] -> T.Text
+buildRow f ids  = T.concat ["<tr>", T.concat (map f ids), "</tr>"]
+
 main :: Fay ()
 main = ready $ do
   buttonCreateField >>= onClick createTable
   buttonSolve >>= onClick initEditor
-  initCursor
+  -- select "body" >>= keydown print
 
   -- Main.load
   -- select "#fielogld table td" >>= onClick start
@@ -43,29 +54,20 @@ createTable _ = do
   rowNum <- select "#rowNum" >>= getVal >>= readInt
   colNum <- select "#colNum" >>= getVal >>= readInt
   select "div.fieldSize input" >>= setAttr "disabled" "disabled"
-  select "#field" >>= append (buildTable rowNum colNum)
+  select "#field" >>= append (buildTable buildCell rowNum colNum)
   buttonCreateField >>= addClass "disabled"
   buttonSolve >>= removeClass "disabled"
   return True
   where
-    buildTable :: Int -> Int -> T.Text
-    buildTable row col = let contents = T.concat $ map buildRow $ chunksOf col [0..(row * col - 1)]
-                         in  T.concat ["<table>", contents, "</table>"]
-
     buildCell :: Int -> T.Text
     buildCell id = T.concat ["<td id=\"c", T.pack (show id), "\"><input type=\"number\"></td>"]
 
-    buildRow :: [Int] -> T.Text
-    buildRow ids  = T.concat ["<tr>", T.concat (map buildCell ids), "</tr>"]
-
 initEditor :: Event -> Fay Bool
 initEditor _ = do
-  -- save
-  select "#field table" >>= remove
-  select "#input" >>= addClass "disabled"
+  -- select "#input" >>= addClass "disabled"
   buttonSolve >>= addClass "disabled"
   initCursor
-  -- load
+  loadField
   return True
 
 buttonCreateField = select "#createTableButton"
@@ -75,18 +77,58 @@ buttonCursor = select "#cursors"
 initCursor :: Fay ()
 initCursor = do
   buttonCursor >>= removeClass "disabled"
-  select "#cursor-up" >>= onClick (dispatch KeyUp)
-  select "#cursor-down" >>= onClick (dispatch KeyDown)
-  select "#cursor-left" >>= onClick (dispatch KeyLeft)
-  select "#cursor-right" >>= onClick (dispatch KeyRight)
+  select "#cursor-up" >>= onClick dispatch
+  select "#cursor-down" >>= onClick dispatch 
+  select "#cursor-left" >>= onClick dispatch 
+  select "#cursor-right" >>= onClick dispatch 
   return ()
   where
-    dispatch :: KeyCode -> Event -> Fay Bool
-    dispatch _ _ = select "body" >>= trigger "keydown" >> return True
+    dispatch :: Event -> Fay Bool
+    dispatch _ = select "body" >>= trigger "keydown" >> return True
 
 
-save :: Event -> Fay Bool
-save = undefined
+loadField :: Fay Bool
+loadField = do
+  rowNum <- select "#rowNum" >>= getVal >>= readInt
+  colNum <- select "#colNum" >>= getVal >>= readInt
+  table <- buildTable buildCell rowNum colNum
+  select "#field table" >>= remove
+  select "#field" >>= append table
+  select "#field > table tbody > tr > td" >>= onClick start
+  return True
+  where
+    cellValue :: Int -> Fay T.Text
+    cellValue cellId = let selector = T.concat ["#c", T.pack $ show cellId]
+                       in  select selector >>= findSelector "input" >>= getVal
+                       -- in  select "#c0" >>= findSelector "input" >>= getVal
+
+    buildTable :: (Int -> Fay T.Text) -> Int -> Int -> Fay T.Text
+    buildTable f row col = do
+      rows <- mapM (buildRow f) $ chunksOf col [0..(row * col - 1)]
+      return $ T.concat ["<table>", T.concat rows, "</table>"]
+
+    buildRow :: (Int -> Fay T.Text) -> [Int] -> Fay T.Text
+    buildRow f ids  = do
+      cells <- mapM f ids 
+      return $ T.concat ["<tr>", T.concat cells, "</tr>"]
+
+    buildCell :: Int -> Fay T.Text
+    buildCell cellId = do
+      value <- cellValue cellId 
+      return $ case value of
+         ""  -> T.concat ["<td id=\"", T.pack $ show cellId, "\">", innerTable, "</td>"]
+         v   -> T.concat ["<td id=\"", T.pack $ show cellId, "\">", value, "</td>"]
+
+    innerTable = "<table class=\"innerTable\"><tr><td></td><td></td></tr><tr><td></td><td></td></tr></table>"
+
+
+start :: Event -> Fay Bool
+start e = do
+  select "#field td" >>= removeClass "current"
+  a <- select "#field"
+  el <- currentTarget e
+  findElement el a >>= addClass "current"
+  return True
 
 getNextId :: Fay ()
 getNextId = undefined
